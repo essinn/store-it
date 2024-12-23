a7228da28751b0d0b59e23cab4ff2d0ebe9a32bd
essinn <ehsan.raj@hotmail.com>
2024-12-23 16:16:10 +0000

    feat: implement reusable auth form and import public assets

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,6 @@
+import AuthForm from "@/components/auth-form";
 import React from "react";
 
-const SignUp = () => {
-  return <div>SignUp</div>;
-};
+const SignUp = () => <AuthForm type="sign-up" />;
 
 export default SignUp;
